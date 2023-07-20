248c8cfe6bf62563899d6842d0404d97fb060592
Shaurya Arora <shaurya.arora1@gmail.com>
2023-07-20 15:49:45 -0400

    Add simulated delay for sleep path

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,8 @@ use std::{
     format, fs,
     io::{BufRead, BufReader, Write},
     net::{TcpListener, TcpStream},
+    thread,
+    time::Duration,
 };
 
 pub fn run() -> std::io::Result<()> {
@@ -24,10 +26,13 @@ fn handle_client(mut stream: TcpStream, resp_body_ok: &str, resp_body_404: &str)
     let buf_reader = BufReader::new(&stream);
     let request_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let (status_line, content) = if request_line == "GET / HTTP/1.1" {
-        ("HTTP/1.1 200 OK", resp_body_ok)
-    } else {
-        ("HTTP/1.1 404 NOT FOUND", resp_body_404)
+    let (status_line, content) = match &request_line[..] {
+        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK", resp_body_ok),
+        "GET /sleep HTTP/1.1" => {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK", resp_body_ok)
+        }
+        _ => ("HTTP/1.1 404 NOT FOUND", resp_body_404),
     };
 
     let response = format!(
